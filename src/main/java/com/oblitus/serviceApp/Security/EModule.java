0c5789d8d410537cbec09e6054b9a761604c5e41
Oblitus <piotr5jurczak@gmail.com>
2023-03-28 00:28:10 +0200

    Add Modules Enumerator for available modules

---
+++ src/main/java/com/oblitus/serviceApp/Security/EModule.java
@@ -0,0 +1,10 @@
+package com.oblitus.serviceApp.Security;
+
+public enum EModule {
+    ADMIN_MODULE,
+    CRM_MODULE,
+    CASH_MODULE,
+    FINANCE_MODULE,
+    PROJECTS_MODULE,
+    SERVICE_MODULE
+}
