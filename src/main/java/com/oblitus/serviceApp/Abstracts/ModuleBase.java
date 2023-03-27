109901393c16724fa08076057a33c76ae4c9a649
Oblitus <piotr5jurczak@gmail.com>
2023-03-28 00:27:41 +0200

    cleaning up Abstract class

--- src/main/java/com/oblitus/serviceApp/Abstracts/ModuleBase.java
+++ src/main/java/com/oblitus/serviceApp/Abstracts/ModuleBase.java
@@ -1,12 +1,13 @@
 package com.oblitus.serviceApp.Abstracts;
 
+import com.oblitus.serviceApp.Security.EModule;
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
 import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @NoArgsConstructor
 public abstract class ModuleBase extends EntityBase {
-    public String moduleName;
-    public List<ModuleElement> Elements;
 
 }
