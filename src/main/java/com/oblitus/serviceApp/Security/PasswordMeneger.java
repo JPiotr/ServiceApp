45fad805777d81b6404bc1e8a7460cf6db0d8fd1
Oblitus <piotr5jurczak@gmail.com>
2023-03-28 15:42:25 +0200

    Basic Password encoding implementation

--- src/main/java/com/oblitus/serviceApp/Security/PasswordMeneger.java
+++ src/main/java/com/oblitus/serviceApp/Security/PasswordMeneger.java
@@ -1,5 +1,17 @@
 package com.oblitus.serviceApp.Security;
 
-public class PasswordMeneger {
+import org.springframework.beans.BeanMetadataElement;
+import org.springframework.beans.factory.xml.ParserContext;
+import org.springframework.security.config.authentication.PasswordEncoderParser;
+import org.w3c.dom.Element;
 
+public class PasswordMeneger extends PasswordEncoderParser {
+    public PasswordMeneger(Element element, ParserContext parserContext) {
+        super(element, parserContext);
+    }
+
+    @Override
+    public BeanMetadataElement getPasswordEncoder() {
+        return super.getPasswordEncoder();
+    }
 }
