c3160ae1ecc4eb936c7e6703a57b7ab5019b058a
Oblitus <piotr5jurczak@gmail.com>
2023-04-03 21:14:18 +0200

    Delete unused imports

--- src/main/java/com/oblitus/serviceApp/Abstracts/ModuleBase.java
+++ src/main/java/com/oblitus/serviceApp/Abstracts/ModuleBase.java
@@ -1,11 +1,6 @@
 package com.oblitus.serviceApp.Abstracts;
 
-import com.oblitus.serviceApp.Security.EModule;
-import jakarta.persistence.Entity;
-import jakarta.persistence.OneToMany;
 import lombok.NoArgsConstructor;
-
-import java.util.List;
 
 @NoArgsConstructor
 public abstract class ModuleBase extends EntityBase {
