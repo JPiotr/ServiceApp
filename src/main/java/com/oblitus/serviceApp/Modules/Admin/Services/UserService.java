8a93b684461969467bca3e5ef730302a0a1fa27b
Oblitus <piotr5jurczak@gmail.com>
2023-04-03 21:16:36 +0200

    Add Crypt to service

--- src/main/java/com/oblitus/serviceApp/Modules/Admin/Services/UserService.java
+++ src/main/java/com/oblitus/serviceApp/Modules/Admin/Services/UserService.java
@@ -15,14 +15,23 @@ import java.util.UUID;
 @AllArgsConstructor
 public class UserService {
     private final UserRepository userRepo;
+    private static final Crypt crypt;
+
+    static {
+        try {
+            crypt = new Crypt();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     public User getUser(UUID id){
         Optional<User> opt = userRepo.findById(id);
         return opt.orElse(null);
     }
 
-    public User addUser(String name, String email, Collection<Role> roles, String password){
-        return userRepo.save(new User(name, email, roles, password));
+    public User addUser(String name, String email, Collection<Role> roles, String password) throws Exception {
+        return userRepo.save(new User(name, email, roles, crypt.cryptData(password)));
     }
 
     public User addUser(User user){
