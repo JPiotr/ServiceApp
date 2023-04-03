85843ff52af6ea29eb6e5180a4ff7f9648331089
Oblitus <piotr5jurczak@gmail.com>
2023-04-03 21:12:08 +0200

    Update Admin Module for business logic

--- src/main/java/com/oblitus/serviceApp/Modules/Admin/AdminModule.java
+++ src/main/java/com/oblitus/serviceApp/Modules/Admin/AdminModule.java
@@ -5,11 +5,21 @@ import com.oblitus.serviceApp.Security.EModule;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
+@AllArgsConstructor
 public final class AdminModule extends ModuleBase {
     private static AdminModule module;
+
     public final EModule Type = EModule.ADMIN_MODULE;
 
+    //for db
     public static ModuleBase getModule(){
+        if(module == null){
+            module = new AdminModule();
+        }
+        return module;
+    }
+
+    public static AdminModule getInstance(){
         if(module == null){
             module = new AdminModule();
         }
