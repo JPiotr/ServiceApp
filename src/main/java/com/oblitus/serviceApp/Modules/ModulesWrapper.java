49928d84183f18dd4f730fd7fe8e55391509acb5
Oblitus <piotr5jurczak@gmail.com>
2023-04-04 02:04:19 +0200

    Add ModulesWrapper singleton

---
+++ src/main/java/com/oblitus/serviceApp/Modules/ModulesWrapper.java
@@ -0,0 +1,27 @@
+package com.oblitus.serviceApp.Modules;
+
+import com.oblitus.serviceApp.Abstracts.ModuleBase;
+import com.oblitus.serviceApp.Modules.Admin.AdminModule;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+public final class ModulesWrapper {
+    private static ModulesWrapper modulesWrapper;
+    private static List<ModuleBase> allModules;
+
+    public ModulesWrapper() {
+        allModules.add(AdminModule.getInstance());
+    }
+
+    public static ModulesWrapper getInstance(){
+        if(modulesWrapper == null){
+            modulesWrapper = new ModulesWrapper();
+        }
+        return modulesWrapper;
+    }
+
+    public static List<ModuleBase> getAllModules() {
+        return allModules;
+    }
+}
