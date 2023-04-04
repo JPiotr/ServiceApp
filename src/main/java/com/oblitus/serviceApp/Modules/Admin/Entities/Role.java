ce902133f58eebdb7f4f8c014944c35c5f7c956f
Oblitus <piotr5jurczak@gmail.com>
2023-04-04 02:03:41 +0200

    Implement SpringSecurity interface for Auth support

--- src/main/java/com/oblitus/serviceApp/Modules/Admin/Entities/Role.java
+++ src/main/java/com/oblitus/serviceApp/Modules/Admin/Entities/Role.java
@@ -1,12 +1,15 @@
 package com.oblitus.serviceApp.Modules.Admin.Entities;
 
 import com.oblitus.serviceApp.Abstracts.EntityBase;
+import com.oblitus.serviceApp.Abstracts.ModuleBase;
 import com.oblitus.serviceApp.Modules.Admin.EModule;
+import com.oblitus.serviceApp.Modules.Admin.ERule;
 import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
 
 import java.util.List;
 
@@ -14,14 +17,23 @@ import java.util.List;
 @NoArgsConstructor
 @Entity
 @Table(name = "roles")
-public class Role extends EntityBase {
-    //todo: available modules?
+public class Role extends EntityBase implements GrantedAuthority {
+
     @ElementCollection
     public List<EModule> Modules;
 
+    public String Name;
+    private ERule roleType;
     //fixme: not List of EModule just list of ModuleBase!
-    public Role(String name, List<EModule> modules) {
-        super(name);
+    public Role(ERule roleType, List<EModule> modules) {
+        super();
+        Name = roleType.toString();
         Modules = modules;
+        this.roleType = roleType;
+    }
+
+    @Override
+    public String getAuthority() {
+        return roleType.toString();
     }
 }
--- src/main/java/com/oblitus/serviceApp/Modules/Admin/Entities/User.java
+++ src/main/java/com/oblitus/serviceApp/Modules/Admin/Entities/User.java
@@ -8,7 +8,10 @@ import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.time.LocalDateTime;
 import java.util.Collection;
 
 //todo: Maybe Root Singleton Class?
@@ -17,15 +20,32 @@ import java.util.Collection;
 @Data
 @Entity
 @Table(name = "users")
-public class User extends EntityBase {
+public class User extends EntityBase implements UserDetails {
     @Column(nullable = false)
     public String Email;
     @OneToMany(targetEntity = Role.class)
     public Collection<Role> Roles;
-    private byte[] password;
 
-    public User(String name, String email, Collection<Role> roles, byte[] password) {
-        super(name);
+    @Column(nullable = false)
+    public String Username;
+
+    public LocalDateTime LastLoginDate;
+
+    public LocalDateTime CredentialExpirationDate;
+
+    public LocalDateTime AccountExpirationDate;
+
+    private boolean Enabled;
+    private boolean Expired;
+    private boolean CredentialsExpired;
+
+    //todo: what do to with password??
+    private String password;
+
+
+    public User(String username, String email, Collection<Role> roles, String password) {
+        super();
+        Username = username;
         Email = email;
         Roles = roles;
         this.password = password;
@@ -39,5 +59,38 @@ public class User extends EntityBase {
     public User deleteRole(Role role){
         Roles.remove(role);
         return this;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return getRoles();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return Expired;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return !super.Locked;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return CredentialsExpired;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return Enabled;
+    }
+
+    public boolean isExpired(){
+        return Expired;
+    }
+
+    public boolean isCredentialsExpired(){
+        return CredentialsExpired;
     }
 }
