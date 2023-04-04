9dd7b846cb4953c6177cd23204ef9cb455c369fb
Oblitus <piotr5jurczak@gmail.com>
2023-04-04 02:05:30 +0200

    Change string to ERule

--- src/main/java/com/oblitus/serviceApp/Modules/Admin/Services/RoleService.java
+++ src/main/java/com/oblitus/serviceApp/Modules/Admin/Services/RoleService.java
@@ -1,6 +1,7 @@
 package com.oblitus.serviceApp.Modules.Admin.Services;
 
 import com.oblitus.serviceApp.Modules.Admin.EModule;
+import com.oblitus.serviceApp.Modules.Admin.ERule;
 import com.oblitus.serviceApp.Modules.Admin.Entities.Role;
 import com.oblitus.serviceApp.Modules.Admin.Repos.RoleRepository;
 import lombok.AllArgsConstructor;
@@ -9,14 +10,14 @@ import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.Optional;
 import java.util.UUID;
-
+//todo: fix typo
 @Service
 @AllArgsConstructor
 public class RoleService {
     private final RoleRepository roleRepository;
 
-    public Role addRole(String name, ArrayList<EModule> modules){
-        return roleRepository.save(new Role(name, modules));
+    public Role addRole(ERule ruleType, ArrayList<EModule> modules){
+        return roleRepository.save(new Role(ruleType, modules));
     }
 
     public Role addRole(Role role){
