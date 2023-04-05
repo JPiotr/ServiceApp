695f5f2e917b10117a152be9630ef2b907cab009
Oblitus <piotr5jurczak@gmail.com>
2023-04-05 22:04:21 +0200

    class moved and update for DAO support

---
+++ src/main/java/com/oblitus/serviceApp/Modules/Admin/DAOs/RuleDAO.java
@@ -0,0 +1,53 @@
+package com.oblitus.serviceApp.Modules.Admin.DAOs;
+
+import com.oblitus.serviceApp.Abstracts.DAO;
+import com.oblitus.serviceApp.Modules.Admin.DTOs.RuleDTO;
+import com.oblitus.serviceApp.Modules.Admin.DTOs.RuleMapper;
+import com.oblitus.serviceApp.Modules.Admin.RuleService;
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+@RequiredArgsConstructor
+public final class RuleDAO implements DAO<RuleDTO> {
+    private final RuleService ruleService;
+    private final RuleMapper ruleMapper;
+
+    @Override
+    public Optional<RuleDTO> get(UUID id) {
+        return Optional.of(
+                ruleMapper.apply(
+                    ruleService.getRule(id)
+            )
+        );
+    }
+
+    @Override
+    public List<RuleDTO> getAll() {
+        return ruleService.getAllRoles()
+                .stream()
+                .map(ruleMapper)
+                .collect(Collectors.toList()
+        );
+    }
+
+    @Override
+    public RuleDTO save(RuleDTO ruleDTO) {
+        return ruleMapper.apply(
+                ruleService.addRule(ruleDTO.name(),ruleDTO.modules())
+        );
+    }
+
+    @Override
+    public RuleDTO update(RuleDTO ruleDTO) {
+        return null;
+    }
+
+    @Override
+    public boolean delete(RuleDTO ruleDTO) {
+        return false;
+    }
+}
