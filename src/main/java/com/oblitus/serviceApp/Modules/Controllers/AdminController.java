1862cb3ed46bf43222dd643d88886f919f2cad5b
Oblitus <piotr5jurczak@gmail.com>
2023-04-06 22:21:12 +0200

    First attempt to add final controller :)

--- src/main/java/com/oblitus/serviceApp/Modules/Controllers/AdminController.java
+++ src/main/java/com/oblitus/serviceApp/Modules/Controllers/AdminController.java
@@ -1,19 +1,48 @@
 package com.oblitus.serviceApp.Modules.Controllers;
 
-import com.oblitus.serviceApp.Modules.Admin.Singletons.AdminModule;
-import com.oblitus.serviceApp.Modules.Admin.User;
-import com.oblitus.serviceApp.Modules.Admin.UserService;
+import com.oblitus.serviceApp.Common.Response;
+import com.oblitus.serviceApp.Modules.Admin.DTOs.UserDTO;
+import com.oblitus.serviceApp.Modules.ModulesWrapper;
 import lombok.AllArgsConstructor;
-import org.springframework.graphql.data.method.annotation.QueryMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.UUID;
 
 @RestController
-@RequestMapping("adminModule")
+@RequestMapping("/adminModule")
 @AllArgsConstructor
 public class AdminController {
-    private static final AdminModule adminModule = AdminModule.getInstance();
+    private static final ModulesWrapper modulesWrapper = ModulesWrapper.getInstance();
 
+    @GetMapping("/users/user")
+    public ResponseEntity<Response> getUser(@RequestBody @Validated UUID id){
+        //todo: obsługa UserDTO z null-em
+        return ResponseEntity.ok(
+                Response.builder()
+                        .timestamp(LocalDateTime.now())
+                        .massage("Getting specyfic user with ID = "+ id)
+                        .data(Map.of("user",ModulesWrapper.adminModule.getAdminDAO().getUserDao().get(id)))
+                        .statusCode(HttpStatus.OK.value())
+                        .status(HttpStatus.OK)
+                        .build()
+        );
+    }
+
+    @GetMapping("/users")
+    public List<UserDTO> getUsers(){
+        return ModulesWrapper.adminModule.getAdminDAO().getUserDao().getAll();
+    }
+
+    @PutMapping("/users")
+    public UserDTO putUser(@RequestBody @Validated UserDTO userDTO){
+        return ModulesWrapper.adminModule.getAdminDAO().getUserDao().save(userDTO);
+    }
 
 }
