4e05e8114110cf4f05e21e913a4ad85a3bfd0696
Oblitus <piotr5jurczak@gmail.com>
2023-05-21 12:35:06 +0200

    fix warnings

--- src/main/java/com/oblitus/serviceApp/Modules/Controllers/AdminController.java
+++ src/main/java/com/oblitus/serviceApp/Modules/Controllers/AdminController.java
@@ -1,10 +1,8 @@
 package com.oblitus.serviceApp.Modules.Controllers;
 
 import com.oblitus.serviceApp.Common.Response;
-import com.oblitus.serviceApp.Modules.Admin.DAOs.UserDAO;
 import com.oblitus.serviceApp.Modules.Admin.DTOs.UserDTO;
 import com.oblitus.serviceApp.Modules.ModulesWrapper;
-import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,7 +11,6 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.security.auth.login.AccountLockedException;
 import java.time.LocalDateTime;
-import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.UUID;
@@ -27,27 +24,24 @@ public class AdminController {
     @GetMapping("/users/user")
     public ResponseEntity<Response> getUser(@RequestBody @Validated UUID id){
         Optional<UserDTO> opt = modulesWrapper.adminModule.getAdminDAO().getUserDao().get(id);
-        if(opt.isEmpty()){
-            return ResponseEntity.ok(
-                    Response.builder()
-                            .timestamp(LocalDateTime.now())
-                            .message("User " + id + " not found.")
-                            .data(Map.of("user",null))
-                            .statusCode(HttpStatus.NOT_FOUND.value())
-                            .status(HttpStatus.NOT_FOUND)
-                            .reason("There is no Entity with this ID!")
-                            .build()
-            );
-        }
-        return ResponseEntity.ok(
+        return opt.<ResponseEntity<Response>>map(userDTO -> ResponseEntity.ok(
                 Response.builder()
                         .timestamp(LocalDateTime.now())
                         .message("User with ID = " + id + ".")
-                        .data(Map.of("user",opt.get()))
+                        .data(Map.of("user", userDTO))
                         .statusCode(HttpStatus.OK.value())
                         .status(HttpStatus.OK)
                         .build()
-        );
+        )).orElseGet(() -> ResponseEntity.ok(
+                Response.builder()
+                        .timestamp(LocalDateTime.now())
+                        .message("User " + id + " not found.")
+                        .data(Map.of("user", null))
+                        .statusCode(HttpStatus.NOT_FOUND.value())
+                        .status(HttpStatus.NOT_FOUND)
+                        .reason("There is no Entity with this ID!")
+                        .build()
+        ));
     }
 
     @GetMapping("/users")
@@ -77,7 +71,7 @@ public class AdminController {
     }
 
     @PostMapping("/user")
-    public ResponseEntity<Response> updateUser(@RequestBody @Validated UserDTO userDTO) throws AccountLockedException {
+    public ResponseEntity<Response> updateUser(@RequestBody @Validated UserDTO userDTO) {
         try {
             return ResponseEntity.ok(
                     Response.builder()
