1a5f4c88f1e87176d18ec2c3d26434f61d6a1596
Oblitus <piotr5jurczak@gmail.com>
2023-05-28 13:33:41 +0200

    Add CRUD service and repo for Comments

---
+++ src/main/java/com/oblitus/serviceApp/Modules/Service/CommentService.java
@@ -0,0 +1,37 @@
+package com.oblitus.serviceApp.Modules.Service;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+@AllArgsConstructor(access = AccessLevel.PUBLIC)
+public class CommentService {
+    private final CommentRepository repository;
+
+    public Optional<Comment> getComment(UUID id){
+        return repository.findById(id);
+    }
+
+    public List<Comment> getAllComments(){
+        return repository.findAll();
+    }
+
+    public Comment addComment(Comment comment){
+        return  repository.save(comment);
+    }
+
+    public boolean deleteComment(UUID id){
+        var opt = repository.findById(id);
+        if(opt.isEmpty()){
+            return false;
+        }
+        repository.delete(opt.get());
+        return true;
+
+    }
+}
