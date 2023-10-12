f283cddd36d39cb2623d0e0aa25fca22d11805c2
Oblitus <piotr5jurczak@gmail.com>
2023-10-12 20:20:34 +0200

    add CommentMapper

---
+++ src/main/java/com/oblitus/serviceApp/Modules/Service/DTOs/CommentMapper.java
@@ -0,0 +1,22 @@
+package com.oblitus.serviceApp.Modules.Service.DTOs;
+
+import com.oblitus.serviceApp.Modules.Admin.DTOs.UserMapper;
+import com.oblitus.serviceApp.Modules.Service.Comment;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.function.Function;
+
+@Service
+@RequiredArgsConstructor
+public class CommentMapper implements Function<Comment, CommentDTO> {
+    private final UserMapper userMapper;
+    @Override
+    public CommentDTO apply(Comment comment) {
+        return new CommentDTO(
+                comment.getID(),
+                comment.getContent(),
+                userMapper.apply(comment.getCreator())
+        );
+    }
+}
