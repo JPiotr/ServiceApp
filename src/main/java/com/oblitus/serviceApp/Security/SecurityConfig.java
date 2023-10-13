d7dada27753e6d1f5e87d95bbafb5fcb1626cccb
Oblitus <piotr5jurczak@gmail.com>
2023-10-13 21:05:07 +0200

    deploy commit

--- src/main/java/com/oblitus/serviceApp/Security/SecurityConfig.java
+++ src/main/java/com/oblitus/serviceApp/Security/SecurityConfig.java
@@ -34,7 +34,7 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authorizationManagerRequestMatcherRegistry -> 
                         authorizationManagerRequestMatcherRegistry.requestMatchers(
-                                        "/auth/**"
+                                        "/**"
                         ).permitAll()
                                 .anyRequest()
                                 .authenticated())
