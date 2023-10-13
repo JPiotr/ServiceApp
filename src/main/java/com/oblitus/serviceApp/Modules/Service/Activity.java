2d60d99a4d1f59dfb10eddc02b31cee982782ee3
Oblitus <piotr5jurczak@gmail.com>
2023-10-13 16:17:39 +0200

    add todo

--- src/main/java/com/oblitus/serviceApp/Modules/Service/Activity.java
+++ src/main/java/com/oblitus/serviceApp/Modules/Service/Activity.java
@@ -2,6 +2,7 @@ package com.oblitus.serviceApp.Modules.Service;
 
 import com.oblitus.serviceApp.Modules.Admin.User;
 
+//todo: implement this and notes to tickets
 public class Activity {
     User user;
 
