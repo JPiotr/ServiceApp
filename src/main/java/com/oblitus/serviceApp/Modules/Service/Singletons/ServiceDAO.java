6ed8a04f6b9b1566c2885343c7ddb870ca0ba6f2
Oblitus <piotr5jurczak@gmail.com>
2023-10-16 17:48:57 +0200

    add ActivityDAO

--- src/main/java/com/oblitus/serviceApp/Modules/Service/Singletons/ServiceDAO.java
+++ src/main/java/com/oblitus/serviceApp/Modules/Service/Singletons/ServiceDAO.java
@@ -1,5 +1,6 @@
 package com.oblitus.serviceApp.Modules.Service.Singletons;
 
+import com.oblitus.serviceApp.Modules.Service.DAOs.ActivityDAO;
 import com.oblitus.serviceApp.Modules.Service.DAOs.ClientDAO;
 import com.oblitus.serviceApp.Modules.Service.DAOs.CommentDAO;
 import com.oblitus.serviceApp.Modules.Service.DAOs.TicketDAO;
@@ -12,6 +13,7 @@ public class ServiceDAO {
     private final ClientDAO CLIENT_DAO;
     private final TicketDAO TICKET_DAO;
     private final CommentDAO COMMENT_DAO;
+    private final ActivityDAO ACTIVITY_DAO;
 
     public ServiceDAO getInstance(){return this;}
 
@@ -20,4 +22,5 @@ public class ServiceDAO {
     public TicketDAO getTicketDao(){return TICKET_DAO;}
 
     public CommentDAO getCommentDao(){return COMMENT_DAO;}
+    public ActivityDAO getActivityDao(){return ACTIVITY_DAO;}
 }
