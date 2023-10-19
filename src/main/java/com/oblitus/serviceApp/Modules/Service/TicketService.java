c03f2d7caf3c1c61829369d1994f1c8cbe1f77a6
Oblitus <piotr5jurczak@gmail.com>
2023-10-19 15:36:53 +0200

    refactor and cleanup, add new feature

--- src/main/java/com/oblitus/serviceApp/Modules/Service/TicketService.java
+++ src/main/java/com/oblitus/serviceApp/Modules/Service/TicketService.java
@@ -1,11 +1,12 @@
 package com.oblitus.serviceApp.Modules.Service;
 
-import com.oblitus.serviceApp.Modules.Service.DTOs.ActivityDTO;
+import com.oblitus.serviceApp.Modules.Admin.User;
+import com.oblitus.serviceApp.Modules.Admin.UserService;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
@@ -16,24 +17,25 @@ public class TicketService {
 
     private final TicketRepository repository;
     private final ActivityRepository activityRepository;
+    private final UserService userService;
 
-    public Optional<Ticket> getTicket(UUID id){
-        return repository.findById(id);
+    public Ticket getTicket(UUID id){
+        var opt = repository.findById(id);
+        if(opt.isPresent()){
+            return opt.get();
+        }
+        throw new EntityNotFoundException();
     }
 
     public List<Ticket> getAllTickets(){
         return repository.findAll();
     }
 
-    public Ticket addTicket(String title, String description, Client client){
-        return repository.save(new Ticket(title,description,client));
-    }
-
     public Ticket addTicket(Ticket ticket){
         return repository.save(ticket);
     }
 
-    public boolean deleteTicket(UUID id){
+    public boolean deleteTicket(UUID id){ //todo: check all deletes
         Optional<Ticket> opt = repository.findById(id);
         if(opt.isEmpty()){
             return false;
@@ -42,22 +44,40 @@ public class TicketService {
         return true;
     }
 
-    public Ticket updateTicket(UUID id, String title, String description, TicketPriority ticketPriority, TicketState ticketState){
-        Optional<Ticket> opt = repository.findById(id);
-        if(opt.isEmpty()){
-            return null;
+    public Ticket updateTicket(UUID id, String title, String description, TicketPriority ticketPriority, TicketState ticketState, UUID assigned, String note, UUID edditingUser){
+        Ticket ticket = getTicket(id);
+        User editor = null;
+        if(edditingUser != null){
+            editor = userService.getUser(edditingUser);
         }
+        User usr = null;
+        if(assigned != null){
+            usr = userService.getUser(assigned);
+            var oldValue = ticket.getCreator();
+            ticket.setCreator(usr);
+            activityRepository.save(new Activity(
+                    EActivityHandle.TICKET.toString(),
+                    "Assigned",
+                    usr.toString(),
+                    oldValue.toString(),
+                    EActivityTypes.SYSTEM.toString(),
+                    editor,
+                    ticket)
+            );
+            ticket.setLastModificationDate();
+        }
+
         if(title != null){
-            opt.get().setTitle(title);
-            opt.get().setLastModificationDate();
+            ticket.setTitle(title);
+            ticket.setLastModificationDate();
         }
         if(description != null){
-            opt.get().setDescription(description);
-            opt.get().setLastModificationDate();
+            ticket.setDescription(description);
+            ticket.setLastModificationDate();
         }
         if(ticketPriority != null){
-            var oldValue = opt.get().getPriority();
-            opt.get().setPriority(ticketPriority);
+            var oldValue = ticket.getPriority();
+            ticket.setPriority(ticketPriority);
 
             activityRepository.save(
                     new Activity(
@@ -66,16 +86,16 @@ public class TicketService {
                             ticketPriority.toString(),
                             oldValue.toString(),
                             EActivityTypes.SYSTEM.toString(),
-                            null,
-                            opt.get()
+                            editor,
+                            ticket
 
                     )
             );
-            opt.get().setLastModificationDate();
+            ticket.setLastModificationDate();
         }
         if(ticketState != null){
-            var oldValue = opt.get().getState();
-            opt.get().setState(ticketState);
+            var oldValue = ticket.getState();
+            ticket.setState(ticketState);
 
             activityRepository.save(
                     new Activity(
@@ -84,13 +104,27 @@ public class TicketService {
                             ticketState.toString(),
                             oldValue.toString(),
                             EActivityTypes.SYSTEM.toString(),
-                            null,
-                            opt.get()
+                            editor,
+                            ticket
                     )
             );
-            opt.get().setLastModificationDate();
+            ticket.setLastModificationDate();
         }
-        return repository.save(opt.get());
+        if(note != null){
+            var oldValue = ticket.getNote();
+            activityRepository.save(
+                    new Activity(
+                            EActivityHandle.TICKET.toString(),
+                            "Note",
+                            note,
+                            oldValue,
+                            EActivityTypes.SYSTEM.toString(),
+                            editor,
+                            ticket
+                    )
+            );
+        }
+        return repository.save(ticket);
 
     }
 
