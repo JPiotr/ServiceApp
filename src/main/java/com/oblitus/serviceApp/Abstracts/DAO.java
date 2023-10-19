96a7262fa7e4ea3bcf8b57a15c969bbaa9de68c0
Oblitus <piotr5jurczak@gmail.com>
2023-10-19 15:29:14 +0200

    add documentation to key interface

--- src/main/java/com/oblitus/serviceApp/Abstracts/DAO.java
+++ src/main/java/com/oblitus/serviceApp/Abstracts/DAO.java
@@ -2,20 +2,20 @@ package com.oblitus.serviceApp.Abstracts;
 
 import javax.security.auth.login.AccountLockedException;
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 
-public interface DAO<R,C> {
+/**
+ *
+ * @param <R> Response record entity
+ * @param <C> Creation record entity
+ */
+public interface DAO<R,C>{
     //C - creation
     //R - response
-    Optional<R> get(UUID id);
-
+    R get(UUID id);
     List<R> getAll();
-
     R save(C c);
-
     R update(C c) throws AccountLockedException;
-
     boolean delete(C c);
     boolean delete(UUID id);
 }
