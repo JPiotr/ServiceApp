c1a09e12706710c403755d7941ffa43e1e4ed124
Oblitus <piotr5jurczak@gmail.com>
2023-10-19 15:40:00 +0200

    extend response entity

--- src/main/java/com/oblitus/serviceApp/Modules/Service/DTOs/ClientResponse.java
+++ src/main/java/com/oblitus/serviceApp/Modules/Service/DTOs/ClientResponse.java
@@ -1,5 +1,7 @@
 package com.oblitus.serviceApp.Modules.Service.DTOs;
 
+import com.oblitus.serviceApp.Modules.Admin.DTOs.BaseUserResponse;
+
 import java.time.LocalDateTime;
 import java.util.UUID;
 
@@ -8,6 +10,7 @@ public record ClientResponse(
         UUID id,
         String name,
         LocalDateTime creationDateTime,
-        LocalDateTime lastModyficatonDateTime
+        LocalDateTime lastModyficatonDateTime,
+        BaseUserResponse creator
 ) {
 }
