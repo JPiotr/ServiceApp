822bfc23ce82c24da5765e5689ff2460b605f879
Oblitus <piotr5jurczak@gmail.com>
2023-10-21 23:22:11 +0200

    fix file info

--- src/main/java/com/oblitus/serviceApp/Modules/Admin/DTOs/BaseUserResponse.java
+++ src/main/java/com/oblitus/serviceApp/Modules/Admin/DTOs/BaseUserResponse.java
@@ -1,11 +1,14 @@
 package com.oblitus.serviceApp.Modules.Admin.DTOs;
 
+import com.oblitus.serviceApp.Common.File.DTOs.FileResponse;
+
 import java.util.UUID;
 
 public record BaseUserResponse(
         UUID id,
         String userName,
         String name,
-        String surname
+        String surname,
+        FileResponse photo
 ) {
 }
