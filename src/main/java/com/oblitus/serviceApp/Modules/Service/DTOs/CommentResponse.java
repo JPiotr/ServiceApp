9a260cff33d168f81258089365ebdb2aef3a90ae
Oblitus <piotr5jurczak@gmail.com>
2023-10-30 12:14:51 +0100

    refactor record to class

--- src/main/java/com/oblitus/serviceApp/Modules/Service/DTOs/CommentResponse.java
+++ src/main/java/com/oblitus/serviceApp/Modules/Service/DTOs/CommentResponse.java
@@ -1,16 +1,20 @@
 package com.oblitus.serviceApp.Modules.Service.DTOs;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.oblitus.serviceApp.Abstracts.BaseResponse;
 import com.oblitus.serviceApp.Modules.Admin.DTOs.BaseUserResponse;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
 
-public record CommentResponse(
-        UUID id,
-        String content,
-        UUID subject,
-        BaseUserResponse creator,
-        LocalDateTime creationDate,
-        LocalDateTime lastModyficationDate
-) {
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class CommentResponse extends BaseResponse {
+        private String content;
+        private UUID subject;
+        private BaseUserResponse creator;
 }
