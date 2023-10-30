6c8271d33618c34bcb7daa095688dbdda5dd7864
Oblitus <piotr5jurczak@gmail.com>
2023-10-30 21:46:43 +0100

    small changes in security

--- src/main/java/com/oblitus/serviceApp/Security/JWT/AuthenticationFilter.java
+++ src/main/java/com/oblitus/serviceApp/Security/JWT/AuthenticationFilter.java
@@ -28,7 +28,7 @@ public class AuthenticationFilter extends OncePerRequestFilter {
             @NonNull HttpServletResponse response,
             @NonNull FilterChain filterChain
     ) throws ServletException, IOException {
-        if(request.getServletPath().contains("/auth")){
+        if(request.getServletPath().contains("/auth/**")){
             filterChain.doFilter(request,response);
             return;
         }
--- src/main/java/com/oblitus/serviceApp/Security/SecurityConfig.java
+++ src/main/java/com/oblitus/serviceApp/Security/SecurityConfig.java
@@ -23,16 +23,13 @@ public class SecurityConfig {
     private final AuthenticationProvider authenticationProvider;
     private static final String[] WHITE_LIST = {
             "/auth/login",
+            "/auth/**",
             "/auth/register",
             "/open-api-docs",
             "/swagger-ui/**",
             "/swagger-ui.html",
             "/actuator",
-
-
-            "/v2/api-docs",
-            "/v3/api-docs",
-            "/v3/api-docs/**",
+            
             "/swagger-resources",
             "/swagger-resources/**",
             "/configuration/ui",
