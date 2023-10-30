5d1742945bbe7d157bdff965ecf6f520f2d75e50
Oblitus <piotr5jurczak@gmail.com>
2023-10-30 12:03:46 +0100

    add BaseResponseMapper class

---
+++ src/main/java/com/oblitus/serviceApp/Abstracts/BaseResponseMapper.java
@@ -0,0 +1,15 @@
+package com.oblitus.serviceApp.Abstracts;
+
+public abstract class BaseResponseMapper<TBuilder extends IBuilder> {
+    protected TBuilder builder;
+    protected TBuilder useBuilder(TBuilder builder){
+        this.builder = builder;
+        return this.builder;
+    }
+
+    protected void chceckBuilderSetted(){
+        if(builder == null){
+            throw new RuntimeException("Builder not setted!");
+        }
+    }
+}
