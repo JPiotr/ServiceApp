8cd010b265241369273ee891040d9996cf535d8c
Oblitus <piotr5jurczak@gmail.com>
2023-10-30 12:15:40 +0100

    refactor wrapper

--- src/main/java/com/oblitus/serviceApp/Modules/ModulesWrapper.java
+++ src/main/java/com/oblitus/serviceApp/Modules/ModulesWrapper.java
@@ -1,17 +1,14 @@
 package com.oblitus.serviceApp.Modules;
 
-import com.oblitus.serviceApp.Abstracts.ModuleBase;
 import com.oblitus.serviceApp.Modules.Admin.Singletons.AdminModule;
 import com.oblitus.serviceApp.Modules.Service.Singletons.ServiceModule;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
-import java.util.ArrayList;
-import java.util.List;
+
 @Component
 @RequiredArgsConstructor
 public final class ModulesWrapper {
-//    public final ModulesWrapper modulesWrapper;
     public final AdminModule adminModule;
     public final ServiceModule serviceModule;
 
