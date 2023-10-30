952eb4259f1f0e02f62281b058c67ea9c57b4412
Oblitus <piotr5jurczak@gmail.com>
2023-10-30 12:11:53 +0100

    add repository custom method

--- src/main/java/com/oblitus/serviceApp/Modules/Service/ActivityRepository.java
+++ src/main/java/com/oblitus/serviceApp/Modules/Service/ActivityRepository.java
@@ -3,8 +3,10 @@ package com.oblitus.serviceApp.Modules.Service;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Collection;
 import java.util.UUID;
 
 @Repository
 public interface ActivityRepository extends JpaRepository<Activity, UUID> {
+    Collection<Activity> findAllByObjectActivity(UUID objectActivity);
 }
