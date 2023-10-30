feea86185070a4a98f7f2ae35ede31a29ff98fbf
Oblitus <piotr5jurczak@gmail.com>
2023-10-30 12:12:26 +0100

    refactor DAO

--- src/main/java/com/oblitus/serviceApp/Modules/Admin/Singletons/AdminDAO.java
+++ src/main/java/com/oblitus/serviceApp/Modules/Admin/Singletons/AdminDAO.java
@@ -1,32 +1,27 @@
 package com.oblitus.serviceApp.Modules.Admin.Singletons;
 
-import com.oblitus.serviceApp.Modules.Admin.DAOs.RuleDAO;
-import com.oblitus.serviceApp.Modules.Admin.DAOs.UserDAO;
+import com.oblitus.serviceApp.Modules.Admin.RuleService;
+import com.oblitus.serviceApp.Modules.Admin.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public final class AdminDAO {
 
-    private final RuleDAO RULE_DAO;
+    private final RuleService RULE_SERVICE;
 
-    private final UserDAO USER_DAO;
-
-//    private final AdminDAO adminDAO;
+    private final UserService USER_SERVICE;
 
     public AdminDAO getInstance(){
         return this;
     }
 
-    public RuleDAO getRuleDao() {
-        return RULE_DAO;
+    public RuleService getRuleService() {
+        return RULE_SERVICE;
     }
 
-    public UserDAO getUserDao() {
-        return USER_DAO;
+    public UserService getUserService() {
+        return USER_SERVICE;
     }
 }
