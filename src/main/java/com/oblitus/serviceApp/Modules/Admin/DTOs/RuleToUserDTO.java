cc81a330b2f779c97c909513d6562801b30d252e
Oblitus <piotr5jurczak@gmail.com>
2023-10-30 12:00:58 +0100

    delete unused module and classes

--- src/main/java/com/oblitus/serviceApp/Modules/Admin/DTOs/RuleToUserDTO.java
+++
@@ -1,7 +0,0 @@
-package com.oblitus.serviceApp.Modules.Admin.DTOs;
-
-public record RuleToUserDTO(
-        String ruleName,
-        boolean operation
-) {
-}
--- src/main/java/com/oblitus/serviceApp/Modules/Project/TaskState.java
+++
@@ -1,8 +0,0 @@
-package com.oblitus.serviceApp.Modules.Project;
-
-public enum TaskState {
-    TODO,
-    DONE,
-    DOING,
-    NEED_CHECK
-}
