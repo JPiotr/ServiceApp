95d65e17fce0731bfa1455258a9a6759e122e4fb
Oblitus <piotr5jurczak@gmail.com>
2023-10-31 11:35:53 +0100

    update wrapper

--- src/main/java/com/oblitus/serviceApp/Modules/ModulesWrapper.java
+++ src/main/java/com/oblitus/serviceApp/Modules/ModulesWrapper.java
@@ -1,6 +1,11 @@
 package com.oblitus.serviceApp.Modules;
 
 import com.oblitus.serviceApp.Modules.Admin.Singletons.AdminModule;
+import com.oblitus.serviceApp.Modules.BaseModule.Singletons.BaseModule;
+import com.oblitus.serviceApp.Modules.Service.DTOs.ActivityResponseMapper;
+import com.oblitus.serviceApp.Modules.Service.DTOs.ClientResponseMapper;
+import com.oblitus.serviceApp.Modules.Service.DTOs.CommentResponseMapper;
+import com.oblitus.serviceApp.Modules.Service.DTOs.TicketResponseMapper;
 import com.oblitus.serviceApp.Modules.Service.Singletons.ServiceModule;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -11,6 +16,7 @@ import org.springframework.stereotype.Component;
 public final class ModulesWrapper {
     public final AdminModule adminModule;
     public final ServiceModule serviceModule;
+    public final BaseModule baseModule;
 
 
 }
