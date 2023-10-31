7ee61697c44883c89d7d0cf6bb50d953adef7ddb
Oblitus <piotr5jurczak@gmail.com>
2023-10-31 11:33:21 +0100

    create response mapper

---
+++ src/main/java/com/oblitus/serviceApp/Modules/BaseModule/DTOs/FileResponseMapper.java
@@ -0,0 +1,29 @@
+package com.oblitus.serviceApp.Modules.BaseModule.DTOs;
+
+import com.oblitus.serviceApp.Abstracts.BaseResponseMapper;
+import com.oblitus.serviceApp.Modules.BaseModule.File;
+import org.springframework.stereotype.Service;
+
+import java.util.function.Function;
+
+@Service
+public class FileResponseMapper extends BaseResponseMapper<FileResponseBuilder> implements Function<File, FileResponse> {
+    @Override
+    public FileResponse apply(File file) {
+        if(file == null){
+            return new FileResponse();
+        }
+        return this.useBuilder(new FileResponseBuilder())
+                .setObjectId(file.getObjectId())
+                .setName(file.getFileName())
+                .setFileExtension(file.getFileExtension())
+                .setDescription(file.getDescription())
+                .setUrl(
+                        "http://localhost:8080/files/"+file.getObjectId()+"/"+file.getFileName()
+                )
+                .setUUID(file.getUuid())
+                .setCreationDate(file.getCreationDate())
+                .setLastModificationDate(file.getLastModificationDate())
+                .build();
+    }
+}
