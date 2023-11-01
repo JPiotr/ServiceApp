ea4b27c3b77593f9d338b292435395d21fcbc77d
Oblitus <piotr5jurczak@gmail.com>
2023-11-01 09:50:16 +0100

    wrap new mappers

--- src/main/java/com/oblitus/serviceApp/Modules/MappersWrapper.java
+++ src/main/java/com/oblitus/serviceApp/Modules/MappersWrapper.java
@@ -1,5 +1,7 @@
 package com.oblitus.serviceApp.Modules;
 
+import com.oblitus.serviceApp.Modules.Admin.Responses.MyProfileResponseMapper;
+import com.oblitus.serviceApp.Modules.Admin.Responses.ProfileResponseMapper;
 import com.oblitus.serviceApp.Modules.BaseModule.DTOs.FileResponseMapper;
 import com.oblitus.serviceApp.Modules.Admin.Responses.UserResponseMapper;
 import com.oblitus.serviceApp.Modules.Service.DTOs.ActivityResponseMapper;
@@ -20,6 +22,8 @@ public final class MappersWrapper {
     public final FileResponseMapper fileMapper;
 
     public final UserResponseMapper userMapper;
+    public final ProfileResponseMapper profileMapper;
+    public final MyProfileResponseMapper myProfileMapper;
 
 
 }
