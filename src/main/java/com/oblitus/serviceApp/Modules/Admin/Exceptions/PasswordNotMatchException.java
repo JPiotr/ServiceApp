7163cb816fc014b3c21db257fc80a424672b50b0
Oblitus <piotr5jurczak@gmail.com>
2023-11-01 09:50:35 +0100

    add custom exceptions

---
+++ src/main/java/com/oblitus/serviceApp/Modules/Admin/Exceptions/PasswordNotMatchException.java
@@ -0,0 +1,8 @@
+package com.oblitus.serviceApp.Modules.Admin.Exceptions;
+
+public class PasswordNotMatchException extends Exception {
+
+    public PasswordNotMatchException(String message) {
+        super(message);
+    }
+}
