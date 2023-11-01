c2487b80baec848ac541b1e611e7347aec3efa33
Oblitus <piotr5jurczak@gmail.com>
2023-11-01 10:55:37 +0100

    create connection to IModelDTO

--- src/main/java/com/oblitus/serviceApp/Abstracts/IService.java
+++ src/main/java/com/oblitus/serviceApp/Abstracts/IService.java
@@ -3,7 +3,7 @@ package com.oblitus.serviceApp.Abstracts;
 import java.util.Collection;
 import java.util.UUID;
 
-public interface IService<TEntity extends EntityBase, TDtoRecord> {
+public interface IService<TEntity extends EntityBase, TDtoRecord extends IModelDTO > {
     TEntity get(TDtoRecord dto);
     Collection<TEntity> getAll();
     TEntity update(TDtoRecord dto);
